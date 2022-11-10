4a0f749bfac111caf506be57e6ab0df4eeb97460
Cedric Vervoord <cedric.immo@live.fr>
2022-11-10 15:35:03 +0100

    lien components card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Card = ({ logement }) => {
-    console.log(logement);
-    return (
-        <div className='globalCard'>
-            <div className='card'>
-                <img src={logement.cover} alt={"photo " + logement.title}></img>
-                <h2 className='cardTitle'>{logement.title}</h2>
-            </div>
+  console.log(logement);
+  return (
+    <Link to={`/fiche_logement/${logement.id}`}>
+      <div className="globalCard">
+        <div className="card">
+          <img src={logement.cover} alt={"photo " + logement.title}></img>
+          <h2 className="cardTitle">{logement.title}</h2>
         </div>
-    );
+      </div>
+    </Link>
+  );
 };
 
 export default Card;
--- src/pages/FicheLogements.js
+++ src/pages/FicheLogements.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const FicheLogements = () => {
-    return (
-        <div>
-            <h1>Logements</h1>
-        </div>
-    );
+  const [card, setData] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetcher = () =>
+      fetch("../../dbLogements.json")
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("20", data);
+          setData(data);
+          console.log("test", card);
+        });
+    fetcher();
+  }, []);
+
+  const logement = card.find((el) => el.id === id);
+  console.log("tab", logement);
+
+  return (
+    <div>
+      <h1>logement</h1>
+      {/* exemple pour workplace */}
+      <h2>{logement.title}</h2>
+      {/* fin exemple */}
+    </div>
+  );
 };
 
 export default FicheLogements;
